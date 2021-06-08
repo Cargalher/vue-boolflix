82f6799d5d68b0c5ce407d1408e6d246b9d1d2e9
Cargalher <cargalher@gmail.com>
2021-06-08 12:26:17 +0200

    axios

--- Milestone_1/main.js
+++ Milestone_1/main.js
@@ -1,11 +1,30 @@
 const app = new Vue ({
     el:'#app',
     data:{
-
+        url: 'https://api.themoviedb.org/3/',
+        query: '',
+        apiKey: '2c02b686abeba1d47393671cb89a17d8',
+        name:'',
+        original_name: '',
+        original_language:'',
+        vote_average: null
     },
     mounted(){
+        const fullUrl = `${this.url}?q=${this.query}&appid=${this.apiKey}`;
+        console.log(fullUrl);
         axios
-        .get('')
+            .get(fullUrl)
+            .then(resp => {
+
+                this.name = resp.data.name;
+                this.original_name = resp.data.original_name;
+                this.original_language = resp.data.original_language;
+                this.vote_average = resp.data.vote_average;
+
+            })
+            // .catch(e => {
+            //     console.error(e);
+            // })
     }
 
 })
